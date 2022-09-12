f2d971ba9d63ef3c65d8ad7970aaac0b580d3f32
sethilakshay <sethilakshay13@gmail.com>
2022-09-12 17:23:13 -0400

    Added Comments in the code and changed formatting for Metrics output array

--- ML Model/majority_vote.py
+++ ML Model/majority_vote.py
@@ -10,6 +10,7 @@ def majority_vote_classifier(infile, outfile):
     data_train = data_train[1:]
     data_test = data_test[1:]
 
+    #Using the Majority vote Classifier method (Prediction value is between 1 and 0)
     def predict (data, col_num):
         cnt_0 = 0
         cnt_1 = 1
@@ -29,19 +30,21 @@ def majority_vote_classifier(infile, outfile):
 
     prediction = predict(data_train, -1)
 
+    #Calculating the training error rate
     def error_cal (data, prediction):
         res = 0
         for i in range(len(data)):
-            if(int(data[i,-1]) == prediction):
+            if(int(data[i,-1]) != prediction):
                 res = res + 1
         return (res/len(data))
 
     error_train = error_cal (data_train, prediction)
     error_test = error_cal (data_test, prediction)
 
+    #Creating Arrays to export the final labels and metrics as txt files
     train_labels = np.full((len(data_train), 1), prediction)
     test_labels = np.full((len(data_test), 1), prediction)
-    metrics = np.array([["error(train): ", round(error_train,5)], ["error(test): ", round(error_test,5)]], dtype=object)
+    metrics = np.array([["error(train):", "%f"%round(error_train,6)], ["error(test):", "%f"%round(error_test,6)]], dtype=object)
 
     np.savetxt('train.txt', train_labels, fmt = "%s")
     np.savetxt('test.txt', test_labels, fmt = "%s")
@@ -53,4 +56,5 @@ if __name__ == '__main__':
     print("The input file is: %s" % (infile))
     print("The output file is: %s" % (outfile))
 
+    #Calling the Majority Vote Classifier function
     majority_vote_classifier(infile, outfile)
